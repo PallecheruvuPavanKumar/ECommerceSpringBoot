1e15b2c028c7d0cf093161fc2f5b34c26df8feaa
P <ppkumar@tekion.com>
2025-12-07 11:50:51 +0530

    AddedSearch

--- src/main/java/com/Ecommerce/controller/ProductController.java
+++ src/main/java/com/Ecommerce/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -31,5 +32,57 @@ public class ProductController {
         }
         else
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    
+    @GetMapping({"/product/{id}/image"})
+    public ResponseEntity<byte[]> getImageById(@PathVariable("id") int id){
+        Product productImage = productService.getProductById(id);
+        if(productImage.getId()>0)
+            return new ResponseEntity<>(productImage.getImageData(),HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    
+    @PostMapping({"/product"})
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile) {
+        try {
+            Product product1 = productService.addOrUpdateProduct(product, imageFile);
+            return new ResponseEntity<>(product1, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @PutMapping({"/product/{id}"})
+    public ResponseEntity<?> updateProduct(@PathVariable int id,@RequestPart Product product, @RequestPart MultipartFile imageFile){
+        try {
+            Product product1 = productService.addOrUpdateProduct(product, imageFile);
+            return new ResponseEntity<>(product1,HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @DeleteMapping({"/product/{id}"})
+    public ResponseEntity<String> deleteProduct(@PathVariable int id){
+        try {
+            Product product = productService.getProductById(id);
+            if (product != null) {
+                productService.deleteProduct(id);
+                return new ResponseEntity<>("Deleted", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Not Found", HttpStatus.NOT_FOUND);
+            }
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+    
+    @GetMapping("/product/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam String keyword) {
+        List<Product> products = productService.searchProducts(keyword);
+        System.out.println("searching with :" + keyword);
+        return new ResponseEntity<>(products, HttpStatus.OK);
     }
 }
--- src/main/java/com/Ecommerce/service/ProductService.java
+++ src/main/java/com/Ecommerce/service/ProductService.java
@@ -4,7 +4,9 @@ import com.Ecommerce.model.Product;
 import com.Ecommerce.repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -19,5 +21,20 @@ public class ProductService {
     
     public Product getProductById(int id) {
         return productRepo.findById(id).orElse(null);
+    }
+    
+    public Product addOrUpdateProduct(Product product, MultipartFile image) throws IOException {
+        product.setImageFile(image.getOriginalFilename());
+        product.setImageType(image.getContentType());
+        product.setImageData(image.getBytes());
+        return productRepo.save(product);
+    }
+    
+    public void deleteProduct(int id) throws IOException {
+        productRepo.deleteById(id);
+    }
+    
+    public List<Product> searchProducts(String keyword){
+        return productRepo.searchProducts(keyword);
     }
 }
