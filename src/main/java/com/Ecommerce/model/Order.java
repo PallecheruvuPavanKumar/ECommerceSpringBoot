731dd1c08ee7c7db93d76c432c8e5da150ef476c
P <ppkumar@tekion.com>
2025-12-08 13:47:33 +0530

    AddedData

--- src/main/java/com/Ecommerce/model/Order.java
+++ src/main/java/com/Ecommerce/model/Order.java
@@ -17,6 +17,7 @@ import java.util.List;
 @AllArgsConstructor
 public class Order {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(unique = true)
     private String orderId;
