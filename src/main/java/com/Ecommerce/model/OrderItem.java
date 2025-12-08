af411b035f24d7dc68851fd240febaab8ffb7e6c
P <ppkumar@tekion.com>
2025-12-08 14:36:18 +0530

    added

--- src/main/java/com/Ecommerce/model/OrderItem.java
+++ src/main/java/com/Ecommerce/model/OrderItem.java
@@ -1,10 +1,7 @@
 package com.Ecommerce.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,6 +16,7 @@ import java.math.BigDecimal;
 @Builder
 public class OrderItem {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     
     @ManyToOne
